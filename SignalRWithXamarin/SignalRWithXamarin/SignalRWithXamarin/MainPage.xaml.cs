49ffb7268ce36d352e5f5a3ec3a64a95a8c298c8
HolguinTech <holguintech@outlook.com>
2021-03-18 07:15:28 -0400

    Project upload

---
+++ SignalRWithXamarin/SignalRWithXamarin/SignalRWithXamarin/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SignalRWithXamarin {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
+            InitializeComponent();
+
+            var signalR = new SignalR.SignalRXamarinTest();
+            var task = Task.Run(async () => await signalR.InitializeAsync());
+            task.Wait();
+        }
+    }
+}
---
+++ SignalRWithXamarin/SignalRWithXamarin/SignalRWithXamarin/SignalR/SignalRXamarinTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRWithXamarin.SignalR {
+    public class SignalRXamarinTest {
+
+        HubConnection connection;
+
+        public Task<bool> InitializeAsync() {
+
+            try {
+                connection = new HubConnectionBuilder()
+                    .WithUrl("https://twentyfoursignalrchatdev.azurewebsites.net/talkhub")
+                    .Build();
+
+                connection.Closed += async (error) => {
+                    await Task.Delay(new Random().Next(0, 5) * 1000);
+                    await connection.StartAsync();
+                };
+            } catch (Exception ex) {
+
+                throw;
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
